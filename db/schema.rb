fdc205f028d9c5fdceee5f6f546d7e403d8a3db2
natalietate <martin.nat@gmail.com>
2017-12-12 16:20:11 -0500

    create user movie table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212210425) do
+ActiveRecord::Schema.define(version: 20171212211913) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "movies_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "movie_id", null: false
+    t.index ["movie_id", "user_id"], name: "index_movies_users_on_movie_id_and_user_id"
+    t.index ["user_id", "movie_id"], name: "index_movies_users_on_user_id_and_movie_id"
   end
 
   create_table "users", force: :cascade do |t|
