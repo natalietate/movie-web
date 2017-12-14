8f808cef624906597b96fba671000e982cbd9ed5
natalietate <martin.nat@gmail.com>
2017-12-14 08:26:06 -0500

    create and show groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,39 @@
 class GroupsController < ApplicationController
+
+  def index
+    @groups = Group.all
+  end
+
+  def show
+    get_group
+  end
+
+  def new
+    get_user
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+      if @group.save
+        redirect_to group_path(@group)
+      else
+        render 'new', notice: 'Oops!'
+      end
+  end
+
+  private
+
+    def get_user
+      @user = User.find_by_id(params[:id])
+    end
+
+    def get_group
+      @group = Group.find(params[:id])
+    end
+
+    def group_params
+      params.require(:group).permit(:title, :description)
+    end
+
 end
