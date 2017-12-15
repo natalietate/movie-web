2aae793a0164e5643b43d192516e40649667ffbe
natalietate <martin.nat@gmail.com>
2017-12-15 14:35:01 -0500

    cached votes to speed up performance

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171215191336) do
+ActiveRecord::Schema.define(version: 20171215193235) do
 
   create_table "group_watchlists", force: :cascade do |t|
     t.integer "group_id"
@@ -33,6 +33,20 @@ ActiveRecord::Schema.define(version: 20171215191336) do
     t.string "poster_path"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "cached_votes_total", default: 0
+    t.integer "cached_votes_score", default: 0
+    t.integer "cached_votes_up", default: 0
+    t.integer "cached_votes_down", default: 0
+    t.integer "cached_weighted_score", default: 0
+    t.integer "cached_weighted_total", default: 0
+    t.float "cached_weighted_average", default: 0.0
+    t.index ["cached_votes_down"], name: "index_movies_on_cached_votes_down"
+    t.index ["cached_votes_score"], name: "index_movies_on_cached_votes_score"
+    t.index ["cached_votes_total"], name: "index_movies_on_cached_votes_total"
+    t.index ["cached_votes_up"], name: "index_movies_on_cached_votes_up"
+    t.index ["cached_weighted_average"], name: "index_movies_on_cached_weighted_average"
+    t.index ["cached_weighted_score"], name: "index_movies_on_cached_weighted_score"
+    t.index ["cached_weighted_total"], name: "index_movies_on_cached_weighted_total"
   end
 
   create_table "user_groups", force: :cascade do |t|
