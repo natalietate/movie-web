e1edbbc28c9594eecd865877aad41e3fd3abfdc2
natalietate <martin.nat@gmail.com>
2017-12-17 13:06:06 -0500

    Can save an event time adn date for a group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -44,7 +44,7 @@ class GroupsController < ApplicationController
     end
 
     def group_params
-      params.require(:group).permit(:title, :description)
+      params.require(:group).permit(:title, :description, :event_time, :event_date)
     end
 
 end
